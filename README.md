3b8cadd9eac1dc92e1474964dc2978104ee99ff4
davydka <davydka@gmail.com>
2018-08-14 17:06:20 -0400

    folder restructuring

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 * using python 3.6
 * `mkvirtualenv --python /Users/<username>/anaconda3/bin/python flask_explorations` or `mkvirtualenv --python /usr/local/bin/python3 flask_explorations`
 * `workon flask_explorations`
-* `pip install Flask-RESTful`
-* `pip install Flask-JWT`
-* `python src/app.py`
+* `pip install -r requirements.txt`
+* `cd <v1 || v2 || v3>`
+* `python app.py`
 * when done, `deactivate`
--- v2/app.py
+++ v2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,13 @@ jwt = JWT(app, authenticate, identity) # create /auth
 items = []
 
 class Items(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='field is required'
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,10 +29,27 @@ class Items(Resource):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': 'item exists'}, 400
 
-        data = request.get_json()
+        data = Items.parser.parse_args()
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
         return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return { 'message': 'item deleted' }
+
+    @jwt_required()
+    def put(self, name):
+        data = Items.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items))
+        if item is None:
+            item = {'name': data['name'], 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemsList(Resource):
     def get(sef):
